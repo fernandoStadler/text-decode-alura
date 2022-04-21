ce556e74a83fd2a68d5e6a2522352b1081282062
fernandoStadler <fernandostadler2@gmail.com>
2022-04-21 19:35:05 -0300

    Integração com frontend

--- app/app.js
+++ app/app.js
@@ -1,40 +1,59 @@
 
-const Regex =  /^[a-z\s]+$/; 
 
-//Regras de negocio e lógica,
+const Regex = /^[a-z\s]+$/;
+var txt_msg = document.getElementById("txt_msg");
+var encrypt_txt = document.getElementById("encrypt_txt");
+var btn_encrypt = document.getElementById("btn_encrypt");
+var btn_decrypt = document.getElementById("btn_decrypt");
+var btn_copy = document.getElementById("btn_copy");
 
-function criptografar(str) {
-    
-    return str
-        .replaceAll("e","enter",)
-        .replaceAll("i","imes")
-        .replaceAll("a","ai")
-        .replaceAll("u","ufat")
-        .replaceAll("o","ober");   
+// Função para criar a criptografia
+
+function encrypt() {
+    var get_msg = txt_msg.value;
+    if (Regex.test(get_msg) == true) {
+       var encrypt_msg = get_msg
+            .replaceAll("e", "enter")
+            .replaceAll("i", "imes")
+            .replaceAll("a", "ai")
+            .replaceAll("u", "ufat")
+            .replaceAll("o", "ober");  
+        return encrypt_txt.innerHTML = encrypt_msg;
+    } else {
+        alert("Digite uma mensagem")
+    }
 }
 
-function descriptografar(str) {
-    
-    return str
-        .replaceAll("enter","e")
-        .replaceAll("imes","i")
-        .replaceAll("ai","a")
-        .replaceAll("ufat","u")
-        .replaceAll("ober","o"); 
+function teste(){
+    alert(encrypt_txt.innerHTML)
 }
 
-//Integração com o frontend
-var texto = "gato";
-var saida = criptografar(texto);
+btn_encrypt.onclick = encrypt;
 
-var descripto = descriptografar("pairaibenterns poberr enterncairair enterssenter dentersaifimesober enter tenterr fimesnailimeszaidober enterlenter coberm sufatcenterssober!");
+// Função para criar a descriptografia
+function decrypt() {
+    var get_msg = encrypt_txt.innerHTML;
+    if (Regex.test(get_msg) == true) {
+      var decrypt_msg =  get_msg
+        .replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ufat", "u")
+        .replaceAll("ober", "o");
+        return encrypt_txt.innerHTML = decrypt_msg;
+    } else {
+        alert("Digite uma mensagem")
+    }
+}
 
-console.log(descripto)
+btn_decrypt.onclick = decrypt;
 
-if(Regex.test(saida)==true) {
-    console.log(saida);
-} else {
-    console.log("Digite uma msg valida"+"SAIDA: "+saida);
- }
+function copy_txt(){
+    var txt_encoded = encrypt_txt.value;
+    txt_encoded.select();
+    document.execCommand("copy");
+    alert(txt_encoded);
+}
 
 
+btn_copy.onclick = copy_txt;
