2d0229180c3c5fa5451357e58863d598b77df88a
fernandoStadler <fernandostadler2@gmail.com>
2022-04-27 11:53:04 -0300

    finalização footer

--- app/app.js
+++ app/app.js
@@ -11,6 +11,10 @@ var btn_decrypt = document.getElementById("btn-descriptography");
 var btn_copy = document.getElementById("btn-copy");
 
 function toasty() {
+
+        box_descritography.classList.add("d-none")
+        msg_figure.classList.remove("d-none")
+
     Toastify({
         text: "A mensagem está fora do padrão aceito, por favor revise o texto.",
         duration: 6000,
@@ -56,24 +60,27 @@ btn_encrypt.onclick = clean_encrypt
 // Função para criar a descriptografia
 function decrypt() {
     var get_msg = txt_msg.value;
-    if (get_msg !== "") {
+    if (Regex.test(get_msg) == true) {
         var decrypt_msg = get_msg
-        .replaceAll("enter", "e")
-        .replaceAll("imes", "i")
-        .replaceAll("ai", "a")
-        .replaceAll("ufat", "u")
-        .replaceAll("ober", "o");
-    return encrypt_txt.value = decrypt_msg;
-    // alert(decrypt_msg);
+            .replaceAll("enter", "e")
+            .replaceAll("imes", "i")
+            .replaceAll("ai", "a")
+            .replaceAll("ufat", "u")
+            .replaceAll("ober", "o");
+        return encrypt_txt.value = decrypt_msg;
+        // alert(decrypt_msg);
     } else {
-        toasty();        
+        toasty();
     }
 }
 // Chama a função de descriptografia e limpa o campo de mensagem
 function clean_decrypt() {
-    decrypt();  
     box_descritography.classList.remove("d-none")
     msg_figure.classList.add("d-none")
+    decrypt();
+
+    txt_msg.value = ""
+    
 }
 btn_decrypt.onclick = clean_decrypt;
 
